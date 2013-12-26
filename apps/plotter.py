0fc8273dec13d986a814550428902652008ee552
Shivek Khurana <khuranashivek@gmail.com>
2013-12-26 13:02:35 +0530

    Finally a fucking plotter

--- apps/plotter.py
+++ apps/plotter.py
@@ -29,7 +29,8 @@ class AccelrationVector(object):
 
 	def _fix_length(self, direction):
 		try:
-			if len(self.a[direction]) > self.max_points : self.a[direction] = []  
+			if len(self.a[direction]) > self.max_points :
+				self.a[direction] = self.a[direction][-self.max_points:]  
 		except KeyError as e:
 			log.error('Direction %s is not defined'%direction)
 	
@@ -66,7 +67,8 @@ class TimeKeeper(object):
 		self.scaling = scaling
 
 	def _fix_length(self):
-		if len(self.points) > self.max_points : self.points = []
+		if len(self.points) > self.max_points : 
+			self.points = self.points[-self.max_points:]
 
 	def tick(self):
 		try:
@@ -79,9 +81,10 @@ class TimeKeeper(object):
 	def truncate(self):
 		self.points = []
 
+
 class Plot(object):
 	"""Hold the real plots"""
-	def __init__(self, figsize=(16,12)):
+	def __init__(self, figsize=(16,12), max_points=20, scaling=4):
 		super(Plot, self).__init__()
 		self.figsize = figsize
 		self.figure = plt.figure(figsize=self.figsize, facecolor='#ffffff')
@@ -90,8 +93,8 @@ class Plot(object):
 			'y':self.figure.add_subplot(312),
 			'z':self.figure.add_subplot(313)
 		}
-		self.max_points = 20.0
-		self.scaling = 4.0
+		self.max_points = max_points
+		self.scaling = scaling
 		self.A = AccelrationVector(max_points=self.max_points)
 		self.T = TimeKeeper(max_points=self.max_points, scaling=self.scaling)
 
@@ -102,9 +105,8 @@ class Plot(object):
 		for acc, axis in self.axes.iteritems():
 			axis.set_title(acc)
 			axis.set_ylim([-2,2])
-
-		self.axes_updates = 0 # log of how many times the axes have been shifted	
-		self.update_axes()
+			axis.set_xlim([1.0/self.T.scaling, self.T.max_points/self.T.scaling])
+			#axis.axis("off")
 
 		plt.ion()
 		plt.tight_layout()
@@ -112,21 +114,19 @@ class Plot(object):
 
 	def update_axes(self):
 		for direction, axis in self.axes.iteritems():
-			try:
-				axis.set_xlim([1.0/self.T.scaling + self.T.points[0], self.T.max_points/self.T.scaling + self.T.points[0]])
-			except IndexError as e:
-				#initially points list is empty
-				axis.set_xlim([1.0/self.T.scaling, self.T.max_points/self.T.scaling])
+			axis.set_xlim([self.T.points[-1], self.T.points[-1]+self.T.scaling*self.T.max_points])
 				
 	def update(self, ax, ay, az):
 		self.A.update([ax, ay, az]) 
 		self.T.tick()
-		i = 0 
+		i = 0  
 		for direction, axis in self.axes.iteritems():
+			if self.T.points[-1] >= self.max_points/self.scaling :
+				axis.set_axis_bgcolor('white')
 			axis.plot(self.T.points , self.A[direction], alpha=0.5, linewidth=0.15)
 		plt.draw()
 
-p = Plot()
+p = Plot(max_points=40, scaling=1)
 data_points = 0
 @gramme.server(3030)
 def plotter(data):
@@ -134,7 +134,9 @@ def plotter(data):
 	try:
 		data = data[:-1].split(',')[2:]
 		data_points += 1
-		if data_points > p.max_points : p.update_axes()
+		if data_points > p.max_points*p.scaling : 
+			p.update_axes()
+			data_points = 0
 		log.info("Data received : %s"%str(data))
 		p.update(float(data[0]), float(data[1]), float(data[2]))
 	except KeyboardInterrupt:
