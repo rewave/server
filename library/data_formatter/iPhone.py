5b34e3384b4ac10defdfd36ac12cbfd9bc42c7a8
Shivek Khurana <khuranashivek@gmail.com>
2014-02-08 08:29:10 +0530

    Sent analysis to standalone repo. Removed library. Need to have domain specific names

--- library/data_formatter/iPhone.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python
-
-import struct
-
-def convert(data):
-	#data is raw packet received by server while using Sensor Streamer by FNI app on iphone
-	return struct.unpack('ffffff', data[4:28])#ax,ay,az,wx,wy,wz
