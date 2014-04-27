2a82ae7fa3832e0dc3477d4ea8324fd658170654
Shivek Khurana <khuranashivek@gmail.com>
2014-04-27 10:45:27 +0530

    All includig match working

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python
 
-import time, csv
-
+import time
 from bluetooth import *
 from logbook import Logger
 from pattern import Pattern
+from pykeyboard import PyKeyboard
 
 log = Logger('main.py', level=0)
 
+k = PyKeyboard()
+
 test_case = Pattern(stream=True) #stream of incoming data
-templates = [Pattern("left_wave", "Left")]#, Pattern("right_wave", "Right"), Pattern("flick_up", "Up")]
+templates = [Pattern(template_name="left_wave", key_code=k.left_key)]#, Pattern("right_wave", "Right"), Pattern("flick_up", "Up")]
+
 
 
 server_sock=BluetoothSocket(RFCOMM)
@@ -21,42 +24,44 @@ port = server_sock.getsockname()[1]
 uuid = "a1a738e0-c3b3-11e3-9c1a-0800200c9a66"
 
 advertise_service(server_sock, "RewaveServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ],  
-                )
+				   service_id = uuid,
+				   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+				   profiles = [ SERIAL_PORT_PROFILE ],  
+				)
 
 def match(test_case, template):
-    return True
+	return True
 
 
 while True:
-    print("Waiting for connection on RFCOMM channel %d" % port)
-    
-    client_sock, client_info = server_sock.accept()
-    print("Accepted connection from ", client_info)
-    
-    try:
-        while True:
-            data = client_sock.recv(2048).decode(encoding='UTF-8')
-            log.debug(data)
-            
-            if (len(test_case) > test_case.max_size):
-                del(test_case[0])
-            test_case.put(data.split(","))
-            
-            for template in templates:
-                if match(test_case, template):
-                    template.execute()
-            
-            if data == "quit":
-                break
-            time.sleep(0.0006)
-    except IOError:
-        pass
-    
-    print("disconnected")
-    
-    client_sock.close()
-    print("all done")
+	print("Waiting for connection on RFCOMM channel %d" % port)
+	
+	try:
+		client_sock, client_info = server_sock.accept()
+		print("Accepted connection from ", client_info)
+	
+	except KeyboardInterrupt:
+		break
+
+	try:
+		while True:
+			data = client_sock.recv(2048).decode(encoding='UTF-8')
+			log.debug(data)
+			test_case.put(data.split(","))
+			
+			for template in templates:
+				if match(test_case, template):
+					k.tap_key(template.key_code)
+			
+			if data == "quit":
+				break
+			time.sleep(0.0006)
+	except IOError:
+		pass
+	
+	print("disconnected")
+	
+	client_sock.close()
+	print("all done")
+
 server_sock.close()
--- pattern.py
+++ pattern.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python
 import csv
-from pykeyboard import PyKeyboard
 
 class Pattern(object):
-	def __init__(self, template_name=None, stream=False, max_size = 40, key=None):
+	def __init__(self, template_name=None, stream=False, max_size = 40, key_code=None):
 		self.template_name = template_name
 		self.pattern_matrix = []
-		
-		if not stream:
-			#only templates will have their keyboard objects
-			self.k = PyKeyboard()
-			self.keycode = k.lookup_character_keycode(key)
+		self.key_code = key_code
 
+		if not stream:
 			with open("templates/"+template_name+".txt", "rb") as csv_file:
 				reader = csv.reader(csv_file)
 				for row in reader:
@@ -29,15 +25,12 @@ class Pattern(object):
 		del(self.pattern_matrix[0])
 		self.pattern_matrix.append(acceleration_instance)
 
-	def execute(self):
-		self.k.tap_key(self.keycode)
-
 def main():
-	letter_s = Pattern("letter_s.txt")
-	print len(letter_s.get())
-
 	stream = Pattern(stream=True)
 	print stream.get()
+
+	left_wave = Pattern(template_name="left_wave", key="Left")
+	print left_wave.get()
 
 if __name__ == '__main__':
 	main()
